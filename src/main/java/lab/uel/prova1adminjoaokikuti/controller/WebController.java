72a4c2bc62136668341fd63e114d1012b91c68fd
Joao V Kikuti <joaovitorkikuti@gmail.com>
2023-03-23 03:03:31 -0300

    att

--- src/main/java/lab/uel/prova1adminjoaokikuti/controller/WebController.java
+++ src/main/java/lab/uel/prova1adminjoaokikuti/controller/WebController.java
@@ -1,11 +1,10 @@
 package lab.uel.prova1adminjoaokikuti.controller;
 
-import lab.uel.prova1adminjoaokikuti.model.ItemCardapio;
 import lab.uel.prova1adminjoaokikuti.model.ItemCardapioRepository;
-import lab.uel.prova1adminjoaokikuti.model.Restaurante;
 import lab.uel.prova1adminjoaokikuti.model.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -18,19 +17,9 @@ public class WebController {
     RestauranteRepository rRepo;
 
     @GetMapping(value = {"/", "index"})
-    public String indexPage() {
-
-        Restaurante rest = new Restaurante();
-        rest.setNome("mae");
-
-        rRepo.save(rest);
-
-        ItemCardapio iCardapio = new ItemCardapio();
-        iCardapio.setNome("lasanha");
-        iCardapio.setDescricao("Muito bom");
-        iCardapio.setPreco(4.70);
-        iCardapio.setRestaurante(rest);
-        iRepo.save(iCardapio);
+    public String indexPage(Model model) {
+        model.addAttribute("restaurantes", rRepo.findAll());
+        model.addAttribute("itens", iRepo.findAll());
 
         return "index";
     }
